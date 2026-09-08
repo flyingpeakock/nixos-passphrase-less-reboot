fd2c9ccb220a16716a615b7001bf9aadf0bee52e
Philip Johansson <philip@phlipphlop.me>
2026-09-08 20:30:03 +0200

    removed all flake inputs

--- flake.nix
+++ flake.nix
@@ -1,26 +1,7 @@
 {
   description = "Reboot your encrypted NixOS system without entering your passphrase";
 
-  outputs =
-    { flake-parts, ... }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        ./shell.nix
-        ./nixosModule.nix
-      ];
-
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-    };
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    git-hooks-nix = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  outputs = _: {
+    nixosModules.default = import ./nixosModule.nix;
   };
 }
--- nixosModule.nix
+++ nixosModule.nix
@@ -1,89 +1,90 @@
 {
-  flake.nixosModules.default =
-    {
-      config,
-      lib,
-      pkgs,
-      ...
-    }:
-    let
-      cfg = config.passphrase-less-reboot;
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.passphrase-less-reboot;
 
-      inherit (lib)
-        mkIf
-        mkForce
-        concatStringsSep
-        optional
-        ;
+  inherit (lib)
+    mkIf
+    mkForce
+    concatStringsSep
+    optional
+    filter
+    attrNames
+    all
+    attrValues
+    ;
 
-      deviceNames = lib.filter (
-        name: config.boot.initrd.luks.devices.${name}.keyFile == cfg.tempKeyFile
-      ) (lib.attrNames config.boot.initrd.luks.devices);
-      getDevice = name: config.boot.initrd.luks.devices.${name}.device;
-    in
-    {
-      options = import ./options.nix lib;
+  deviceNames = filter (name: config.boot.initrd.luks.devices.${name}.keyFile == cfg.tempKeyFile) (
+    attrNames config.boot.initrd.luks.devices
+  );
+  getDevice = name: config.boot.initrd.luks.devices.${name}.device;
+in
+{
+  options = import ./options.nix lib;
 
-      config = mkIf cfg.enable {
-        assertions = [
-          {
-            assertion = deviceNames != [ ];
-            message = ''
-              No LUKS devices found with keyFile set to ${cfg.tempKeyFile}.
-              Set `config.boot.initrd.luks.devices.*.keyFile` to ${cfg.tempKeyFile}
-              for at least one device to use passphrase-less reboot.
-            '';
-          }
-          {
-            assertion =
-              config.boot.initrd.systemd.enable
-              || (lib.all (deviceName: config.boot.initrd.luks.${deviceName}.fallbackToPassword) deviceNames);
-            message = ''
-              Without systemd in the stage-1 initrd, all configured LUKS devices must have
-              `fallbackToPassword = true`.
-            '';
-          }
-        ];
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = deviceNames != [ ];
+        message = ''
+          No LUKS devices found with keyFile set to ${cfg.tempKeyFile}.
+          Set `config.boot.initrd.luks.devices.*.keyFile` to ${cfg.tempKeyFile}
+          for at least one device to use passphrase-less reboot.
+        '';
+      }
+      {
+        assertion =
+          config.boot.initrd.systemd.enable
+          || (all (deviceName: config.boot.initrd.luks.${deviceName}.fallbackToPassword) deviceNames);
+        message = ''
+          Without systemd in the stage-1 initrd, all configured LUKS devices must have
+          `fallbackToPassword = true`.
+        '';
+      }
+    ];
 
-        systemd.services.prepare-kexec = {
-          path = lib.attrValues {
-            inherit (pkgs)
-              cpio
-              cryptsetup
-              gzip
-              ;
-          };
-
-          script = mkForce (
-            import ./script.nix {
-              inherit
-                concatStringsSep
-                deviceNames
-                getDevice
-                cfg
-                ;
-            }
-          );
-        };
-
-        boot.initrd.systemd.services.clear-luks-keyslot = {
-          description = "Clear the temporary LUKS keyslot after kexec";
-          wantedBy = [ "initrd.target" ];
-          after = map (name: "systemd-cryptsetup@${name}.service") deviceNames;
-          serviceConfig.Type = "oneshot";
-          path = [ pkgs.cryptsetup ];
-          script = concatStringsSep "\n" (
-            map (name: ''
-              cryptsetup luksKillSlot --batch-mode ${getDevice name} ${toString cfg.keySlot} || true
-            '') deviceNames
-          );
-        };
-
-        environment.systemPackages = optional cfg.installPackage (
-          pkgs.writeShellScriptBin "kexec-reboot" ''
-            systemctl start prepare-kexec && systemctl kexec
-          ''
-        );
+    systemd.services.prepare-kexec = {
+      path = attrValues {
+        inherit (pkgs)
+          cpio
+          cryptsetup
+          gzip
+          ;
       };
+
+      script = mkForce (
+        import ./script.nix {
+          inherit
+            concatStringsSep
+            deviceNames
+            getDevice
+            cfg
+            ;
+        }
+      );
     };
+
+    boot.initrd.systemd.services.clear-luks-keyslot = {
+      description = "Clear the temporary LUKS keyslot after kexec";
+      wantedBy = [ "initrd.target" ];
+      after = map (name: "systemd-cryptsetup@${name}.service") deviceNames;
+      serviceConfig.Type = "oneshot";
+      path = [ pkgs.cryptsetup ];
+      script = concatStringsSep "\n" (
+        map (name: ''
+          cryptsetup luksKillSlot --batch-mode ${getDevice name} ${toString cfg.keySlot} || true
+        '') deviceNames
+      );
+    };
+
+    environment.systemPackages = optional cfg.installPackage (
+      pkgs.writeShellScriptBin "kexec-reboot" ''
+        systemctl start prepare-kexec && systemctl kexec
+      ''
+    );
+  };
 }
